98eaaa1d7f8a09d62c12b5a3a74659f445eeea9d
recanman <29310982+recanman@users.noreply.github.com>
2026-07-27 16:26:04 +0300

    refactor: use `BindArguments`

--- pkg/pce/clusters.go
+++ pkg/pce/clusters.go
@@ -87,7 +87,15 @@ func GetClusterHardware() (mcp.Tool, server.ToolHandlerFunc) {
 }
 
 func handleGetClusterHardware(ctx context.Context, req mcp.CallToolRequest) (*mcp.CallToolResult, error) {
-	clusterId, _ := optionalParam[string](req, "cluster_id")
+	type reqType struct {
+		ClusterId string `json:"cluster_id"`
+	}
+
+	args := &reqType{}
+	if err := req.BindArguments(args); err != nil {
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+	clusterId := args.ClusterId
 
 	client, err := clientForRequest(ctx, req)
 	if err != nil {
